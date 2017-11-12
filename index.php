dbaf9e32c6bc725df28437effc606270a3c4dd68
Serg <venediktovsk1990@mail.ru>
2017-11-12 17:27:23 +0300

    checked and maked cleaning code according with master suggestion

--- index.php
+++ index.php
@@ -84,11 +84,11 @@ $lot_time_remaining = date("H:i", $tomorrow - $now);
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-			<?php for($i=0; $i<count($categories); $i++):  ?>
+			<?php foreach( $categories as $valueCat):  ?>
 				<li class="promo__item promo__item--boards">
-					<a class="promo__link" href="all-lots.html"> <?php print($categories[$i]); ?></a>
+					<a class="promo__link" href="all-lots.html"> <?=$valueCat; ?></a>
 				</li>
-			<?php endfor; ?>
+			<?php endforeach; ?>
         </ul>
     </section>
     <section class="lots">
@@ -96,18 +96,18 @@ $lot_time_remaining = date("H:i", $tomorrow - $now);
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-			<?php for($i=0; $i<count($lots); $i++):  ?>
+			<?php foreach( $lots as $lot ):  ?>
 				<li class="lots__item lot">
 					<div class="lot__image">
-						<img src=<?=$lots[$i]['img'] ?> width="350" height="260" alt="Сноуборд">
+						<img src=<?=$lot['img'] ?> width="350" height="260" alt="Сноуборд">
 					</div>
 					<div class="lot__info">
-						<span class="lot__category"><?php print($lots[$i]['category']) ?></span>
-						<h3 class="lot__title"><a class="text-link" href="lot.html"> <?php print($lots[$i]['name']) ?> </a></h3>
+						<span class="lot__category"><?=$lot['category']; ?></span>
+						<h3 class="lot__title"><a class="text-link" href="lot.html"> <?=$lot['name']; ?> </a></h3>
 						<div class="lot__state">
 							<div class="lot__rate">
 								<span class="lot__amount">Стартовая цена</span>
-								<span class="lot__cost"> <?php print($lots[$i]['cost']) ?> <b class="rub">р</b></span>
+								<span class="lot__cost"> <?=$lot['cost']; ?> <b class="rub">р</b></span>
 							</div>
 							<div class="lot__timer timer">
 								<?=$lot_time_remaining;?>
@@ -116,7 +116,7 @@ $lot_time_remaining = date("H:i", $tomorrow - $now);
 					</div>
 				</li>
 				
-			<?php endfor; ?>
+			<?php endforeach; ?>
         </ul>
     </section>
 </main>
