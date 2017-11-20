abad3e0afd3873e44fb87da3f0a29b4454e0c3f2
Serg <venediktovsk1990@mail.ru>
2017-11-20 22:04:46 +0300

    module4-task1 has done

--- data.php
+++ data.php
@@ -9,5 +9,31 @@
 	[ 'name'=>'Маска Oakley Canopy', 'category'=>'Разное', 'cost'=>'5400', 'img'=>'img/lot-6.jpg'],
  ];
  
- 
+ // ставки пользователей, которыми надо заполнить таблицу
+$bets = [
+    ['name' => 'Иван', 'price' => 11500, 'ts' => strtotime('-' . rand(1, 50) .' minute')],
+    ['name' => 'Константин', 'price' => 11000, 'ts' => strtotime('-' . rand(1, 18) .' hour')],
+    ['name' => 'Евгений', 'price' => 10500, 'ts' => strtotime('-' . rand(25, 50) .' hour')],
+    ['name' => 'Семён', 'price' => 10000, 'ts' => strtotime('last week')]
+];
+
+$is_auth = (bool) rand(0, 1);
+
+$user_name = 'Константин';
+$user_avatar = 'img/user.jpg';
+
+// устанавливаем часовой пояс в Московское время
+date_default_timezone_set('Europe/Moscow');
+
+// записать в эту переменную оставшееся время в этом формате (ЧЧ:ММ)
+$lot_time_remaining = "00:00";
+
+// временная метка для полночи следующего дня
+$tomorrow = strtotime('tomorrow midnight');
+
+// временная метка для настоящего времени
+$now = strtotime('now');
+
+// далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
+$lot_time_remaining=date("H:i", $tomorrow - $now);
  ?>
--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php
-$is_auth = (bool) rand(0, 1);
 
-$user_name = 'Константин';
-$user_avatar = 'img/user.jpg';
-
-// устанавливаем часовой пояс в Московское время
-date_default_timezone_set('Europe/Moscow');
-
-// записать в эту переменную оставшееся время в этом формате (ЧЧ:ММ)
-$lot_time_remaining = "00:00";
-
-// временная метка для полночи следующего дня
-$tomorrow = strtotime('tomorrow midnight');
-
-// временная метка для настоящего времени
-$now = strtotime('now');
-
-// далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
-$lot_time_remaining=date("H:i", $tomorrow - $now);
  
  require_once('./data.php');
 
