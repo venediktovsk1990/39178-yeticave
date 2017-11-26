217eac2bd6c0f87a6a1b2ac96f43701966ef550f
Serg <venediktovsk1990@mail.ru>
2017-11-26 16:00:44 +0300

    fix commit's errors

--- add.php
+++ add.php
@@ -5,48 +5,64 @@ $page_content='';
 $layout_content='';
 
 	//валидация формы
-	
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	
 
 		$required=['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
-		$dict=['lot-name'=>'Название', 'category'=>'Категория', 'message'=>'Описание', 'lot-rate'=>'Начальная цена', 'lot-step'=>'Шаг ставки', 'lot-date'=>'Дата окончания'];
+		$math=['lot-rate', 'lot-step'];
+		$dict=['lot-name'=>'Название', 'category'=>'Категория', 'message'=>'Описание', 'lot-rate'=>'Начальная цена', 'lot-step'=>'Шаг ставки', 'lot-date'=>'Дата окончания торгов'];
 		$error=[];
 		$gif = $_POST;
 		foreach( $_POST as $key=>$value){
 			if( in_array($key, $required)  ){
 				if( !$value ){
-				
 					$error[$dict[$key]] = 'Это поле надо заполнить';
+				}
+				if( ($key=='category') && ( !in_array($value, $categories) ) ){
+					$error[$dict[$key]] = 'Выбирите категорию';
+				}
+			}
+			
+			//проверка что ввели только числа
+			if( $value && in_array($key, $math)){
+				if( !is_numeric($value) ){
+					$error[$dict[$key]] = 'В это поле нужно вводить только числовые значения';
 				}
 			}
 		}
 		
-		print( count($error) );
-		if (isset($_FILES['gif_img']['name'])) {
-			$tmp_name = $_FILES['gif_img']['tmp_name'];
-			$path = $_FILES['gif_img']['name'];
+		//проверка загрузки файла
+		print( isset( $_FILES['jpg_img']['name'] )); //не нормальное поведение при повторной отправки формы на проверке без загрузки файла.
+		if ( (isset( $_FILES['jpg_img']['name']) ) && ( $_FILES['jpg_img']['name'] != "" ) ) {
+			$tmp_name = $_FILES['jpg_img']['tmp_name'];
+			$path = $_FILES['jpg_img']['name'];
 
 			$finfo = finfo_open(FILEINFO_MIME_TYPE);
 			$file_type = finfo_file($finfo, $tmp_name);
-			if ($file_type !== "image/png") {
-				$error['Файл'] = 'Загрузите картинку в формате GIF';
+			if ($file_type !== "image/jpeg") {
+				$error['Файл'] = 'Загрузите картинку в формате jpg';
 			}
 			else {
-				move_uploaded_file($tmp_name, 'img/' . $path);
-				$gif['path'] = $path;
+				move_uploaded_file($tmp_name, "./img/" . $path);
+				$gif['path'] =( "/img/" . $path);
+				
 			}
 		}else{
 			$error['Файл']="Вы не загрузили файл";
 		}
 		
+		
+		//в зависимости от валидации выводим разные страницы
 		$form_values = $gif;
 		if( count($error) ){
 			$page_content=includeTemplate('./templates/add_lot_temp.php', ['categories'=>$categories, 'form_values'=>$form_values, 'errors'=>$error,] );
 		}else{
-
+		
 			$lots[] = [ 'name'=>strip_tags($form_values['lot-name']), 'category'=>$form_values['category'], 'cost'=>strip_tags($form_values['lot-rate']), 'img'=>$form_values['path'] ];
 			$lotIndex = count($lots)-1;
 			$page_content=includeTemplate('./templates/lot.php', ['categories'=>$categories, 'lot'=>$lots[$lotIndex], 'bets'=>$bets,] );
+			$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>$lots[$lotIndex]['name']]  );
+			print($layout_content);
 		}
 		
 	}else{
