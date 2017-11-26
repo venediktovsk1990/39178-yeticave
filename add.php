eca7f2163ae1614229f4dc2615f8ade9d17e5ae4
Serg <venediktovsk1990@mail.ru>
2017-11-26 18:17:23 +0300

    fix checking of user data

--- add.php
+++ add.php
@@ -12,7 +12,7 @@ $layout_content='';
 		$math=['lot-rate', 'lot-step'];
 		$dict=['lot-name'=>'Название', 'category'=>'Категория', 'message'=>'Описание', 'lot-rate'=>'Начальная цена', 'lot-step'=>'Шаг ставки', 'lot-date'=>'Дата окончания торгов'];
 		$error=[];
-		$gif = $_POST;
+		$form_values = $_POST;
 		foreach( $_POST as $key=>$value){
 			if( in_array($key, $required)  ){
 				if( !$value ){
@@ -23,10 +23,16 @@ $layout_content='';
 				}
 			}
 			
+			//удаление из пользовательского ввода нежелательных символов
+			$value = strip_tags($value);
+			
+			
 			//проверка что ввели только числа
 			if( $value && in_array($key, $math)){
 				if( !is_numeric($value) ){
 					$error[$dict[$key]] = 'В это поле нужно вводить только числовые значения';
+				}else{
+					$value = (int)$value;
 				}
 			}
 		}
@@ -39,12 +45,12 @@ $layout_content='';
 
 			$finfo = finfo_open(FILEINFO_MIME_TYPE);
 			$file_type = finfo_file($finfo, $tmp_name);
-			if ($file_type !== "image/jpeg") {
-				$error['Файл'] = 'Загрузите картинку в формате jpg';
+			if ( ($file_type !== "image/jpeg") && ($file_type !== "image/png") && ($file_type !== "image/gif") ) {
+				$error['Файл'] = 'Загрузите картинку';
 			}
 			else {
 				move_uploaded_file($tmp_name, "./img/" . $path);
-				$gif['path'] =( "/img/" . $path);
+				$form_values['path'] =( "/img/" . $path);
 				
 			}
 		}else{
@@ -53,12 +59,13 @@ $layout_content='';
 		
 		
 		//в зависимости от валидации выводим разные страницы
-		$form_values = $gif;
 		if( count($error) ){
 			$page_content=includeTemplate('./templates/add_lot_temp.php', ['categories'=>$categories, 'form_values'=>$form_values, 'errors'=>$error,] );
+			$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>'Добавление нового лота'] );
+			print($layout_content);
 		}else{
 		
-			$lots[] = [ 'name'=>strip_tags($form_values['lot-name']), 'category'=>$form_values['category'], 'cost'=>strip_tags($form_values['lot-rate']), 'img'=>$form_values['path'] ];
+			$lots[] = [ 'name'=>$form_values['lot-name'], 'category'=>$form_values['category'], 'cost'=>$form_values['lot-rate'], 'img'=>$form_values['path'] ];
 			$lotIndex = count($lots)-1;
 			$page_content=includeTemplate('./templates/lot.php', ['categories'=>$categories, 'lot'=>$lots[$lotIndex], 'bets'=>$bets,] );
 			$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>$lots[$lotIndex]['name']]  );
@@ -67,9 +74,10 @@ $layout_content='';
 		
 	}else{
 		$page_content=includeTemplate('./templates/add_lot_temp.php', ['categories'=>$categories, [] ] );
-	}
 		$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>'Добавление нового лота'] );
 		print($layout_content);
+	}
+		
 	
  
 ?>
