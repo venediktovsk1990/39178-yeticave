53c2e5d618457d9bcc23fa5eac38fe65f2db1506
Serg <venediktovsk1990@mail.ru>
2017-12-10 20:24:10 +0300

    save files

--- index.php
+++ index.php
@@ -5,10 +5,43 @@
  require_once('./functions.php');
  require_once('./init.php');
  
- $page_content=includeTemplate('./templates/index.php', ['categories'=>$categories, 'lots'=>$lots, 'lot_time_remaining'=>$lot_time_remaining] );
- 
- $layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>'Главная']  );
- 
- print($layout_content);
+	 if (!$link) {
+		$error = mysqli_connect_error();
+		$page_content = include_template('templates/error_temp.php', ['error_text' => $error]);
+		$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'categories'=>[], 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>'Главная']  );
+		print($layout_content);
+		exit();
+	}
+	//получаем категории
+	$sql = 'SELECT `title` FROM categories';
+	$result = mysqli_query($link, $sql);
+	if( !$result ){
+		$error = mysqli_error($link);
+		$page_content = include_template('templates/error_temp.php', ['error_text' => $error]);
+		$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'categories'=>[], 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>'Главная']  );
+		print($layout_content);
+		exit();
+	}
+	$categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	
+	//получаем все лоты
+	$sql = 'SELECT * FROM lots';
+	$result = mysqli_query($link, $sql);
+	if( !$result ){
+		$error = mysqli_error($link);
+		$page_content = include_template('templates/error_temp.php', ['error_text' => $error]);
+		$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'categories'=>[], 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>'Главная']  );
+		print($layout_content);
+		exit();
+	}
+	$lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	
+	$page_content=includeTemplate('./templates/index.php', ['categories'=>$categories, 'lots'=>$lots, 'lot_time_remaining'=>$lot_time_remaining] );
+	$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'categories'=>$categories, 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>'Главная']  );
+	print($layout_content);
+	
+
+	 
+	 
  
 ?>
--- templates/index.php
+++ templates/index.php
@@ -2,11 +2,15 @@
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-			<?php foreach( $categories as $valueCat):  ?>
+			<?php if( isset($categories) ):
+					foreach( $categories as $category):  ?>
 				<li class="promo__item promo__item--boards">
-					<a class="promo__link" href="all-lots.html"> <?=$valueCat; ?></a>
+					<a class="promo__link" href="all-lots.html"> <?=$category['title']; ?></a>
 				</li>
-			<?php endforeach; ?>
+			<?php 
+				endforeach; 
+			endif;
+			?>
         </ul>
     </section>
     <section class="lots">
@@ -14,14 +18,14 @@
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-			<?php foreach( $lots as $key=>$lot ):  ?>
+			<?php foreach( $lots as $lot ):  ?>
 				<li class="lots__item lot">
 					<div class="lot__image">
 						<img src=<?=$lot['img'] ?> width="350" height="260" alt="Сноуборд">
 					</div>
 					<div class="lot__info">
 						<span class="lot__category"><?=$lot['category']; ?></span>
-						<h3 class="lot__title"><a class="text-link" href=<?=sprintf("\"lot.php?lotIndex=%d\"", $key);?> "> <?=$lot['name']; ?> </a></h3>
+						<h3 class="lot__title"><a class="text-link" href=<?=sprintf("\"lot.php?lotIndex=%d\"", $key);?> "> <?=$lot['title']; ?> </a></h3>
 						<div class="lot__state">
 							<div class="lot__rate">
 								<span class="lot__amount">Стартовая цена</span>
