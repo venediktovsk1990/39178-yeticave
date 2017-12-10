1896765a0923d2f5263fa54955afa9ba662cb2a7
Serg <venediktovsk1990@mail.ru>
2017-12-10 13:47:06 +0300

    add scenario for db connection

--- init.php
+++ init.php
@@ -1,11 +1,26 @@
 <?php
 
+$page_content='';
+$layout_content='';
+
 	session_start();
 	if(isset($_SESSION['user']) ){
 		$user_name = $_SESSION['user']['name'];
 		$is_auth = true;
 	}
 
-
+	//подключение к БД
+	$link = @mysqli_connect("localhost", "root", "", "yeticave");
+	if( !$link ){
+		$error_msg = 'Произошла ошибка соединения с БД - ' . mysqli_connect_error();
+		$page_content= includeTemplate('./templates/error_temp.php', [ 'error_text'=>$error_msg ] );
+		$layout_content=includeTemplate('./templates/layout.php', ['main_content'=>$page_content, 'is_auth'=>$is_auth, 'user_name'=>$user_name, 'user_avatar'=>$user_avatar, 'title'=>'Ошибка подключения в БД']  );
+		print($layout_content);	
+	
+		exit();
+	}else{
+	
+		mysqli_set_charset( $link, "utf8");
+	}
 
 ?>
